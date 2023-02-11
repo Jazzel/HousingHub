34f38f7a5e9868e9863cb007acaa35b9515fed61
Muhammad Jazzel Mehmood <jazzelmehmood4@gmail.com>
2023-02-11 10:38:56 +0500

    Routing setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
+import store from "./store";
+import { Provider } from "react-redux";
+import {
+  Route,
+  BrowserRouter as Router,
+  Routes,
+  useLocation,
+} from "react-router-dom";
+import { useLayoutEffect } from "react";
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+const Wrapper = ({ children }) => {
+  const location = useLocation();
+  useLayoutEffect(() => {
+    document.documentElement.scrollTo(0, 0);
+  }, [location.pathname]);
+  return children;
+};
+
 const App = () => {
-  return <div className="App">HousingHub</div>;
+  return (
+    <Provider store={store}>
+      <Router>
+        <Wrapper>
+          <Routes>
+            <Route path="/*">
+              <Route index element={<Home />} />
+              <Route path="about" element={<About />} />
+            </Route>
+          </Routes>
+        </Wrapper>
+      </Router>
+    </Provider>
+  );
 };
 
 export default App;
